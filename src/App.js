42984539f39912822929bed9d829f85e5c1bb473
Lloyd Chambrier <lloydchambrier@gmail.com>
2020-03-10 15:11:14 +0100

    Added useState Hooks to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="left-pane">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form>
+          <input type="text" label="Username" value={username} onChange={e => setUsername(e.target.value)}/>
+          <input type="password" label="Password" value={password} onChange={e => setPassword(e.target.value)}/>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      
+      <div className="right-pane">
+        {username && <p> Username: {username}</p>}
+        {password && <p>Password: {password}</p>}
+      </div>
     </div>
   );
 }
