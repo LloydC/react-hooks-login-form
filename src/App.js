947768c033ecd8cbd810079ec06738fd3b92804a
Lloyd Chambrier <lloydchambrier@gmail.com>
2020-03-10 15:29:22 +0100

    Added useReducer hook to App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, {useState} from 'react';
+import React, {useState, useReducer} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  
+  // const [username, setUsername] = useState('');
+  // const [password, setPassword] = useState('');
+  const [state, updateState] = useReducer(
+    (state, newState) => ({...state, ...newState}), {username:'', password:''}
+  )
+
+  const {username, password} = state;
+
   return (
     <div className="App">
       <div className="left-pane">
         <img src={logo} className="App-logo" alt="logo" />
         <form>
-          <input type="text" label="Username" value={username} onChange={e => setUsername(e.target.value)}/>
-          <input type="password" label="Password" value={password} onChange={e => setPassword(e.target.value)}/>
+          <input type="text" label="Username" value={username} onChange={e => updateState({username: e.target.value})}/>
+          <input type="password" label="Password" value={password} onChange={e => updateState({password: e.target.value})}/>
           <button type="submit">Submit</button>
         </form>
       </div>
